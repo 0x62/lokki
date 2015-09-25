1890f9d0625e25274e75628bc712d0ac7003beb4
Benedict Lewis <benedict@ovalbit.com>
2015-09-25 19:09:46 +0100

    Updated auth controller to support Facebook only

--- config/secrets.js
+++ config/secrets.js
@@ -1,29 +1,8 @@
-/**
- * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
- *
- * You should never commit this file to a public repository on GitHub!
- * All public code on GitHub can be searched, that means anyone can see your
- * uploaded secrets.js file.
- *
- * I did it for your convenience using "throw away" API keys and passwords so
- * that all features could work out of the box.
- *
- * Use config vars (environment variables) below for production API keys
- * and passwords. Each PaaS (e.g. Heroku, Nodejitsu, OpenShift, Azure) has a way
- * for you to set it up from the dashboard.
- *
- * Another added benefit of this approach is that you can use two different
- * sets of keys for local development and production mode without making any
- * changes to the code.
-
- * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
- */
-
 module.exports = {
 
-  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
+  db: process.env.MONGODB || process.env.MONGO || 'mongodb://localhost:27017/lokki',
 
-  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
+  sessionSecret: process.env.SESSION_SECRET || 'z+NkEftt5ZarcOI2s3HqLrGsUYk4j1Z1ztxQdIwYIDQ',
 
   mailgun: {
     user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
@@ -52,7 +31,7 @@ module.exports = {
   facebook: {
     clientID: process.env.FACEBOOK_ID || '754220301289665',
     clientSecret: process.env.FACEBOOK_SECRET || '41860e58c256a3d7ad8267d3c1939a4a',
-    callbackURL: '/auth/facebook/callback',
+    callbackURL: '/auth/callback',
     passReqToCallback: true
   },
 
